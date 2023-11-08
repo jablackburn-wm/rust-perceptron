03e647179adf79992b5eca5d674e41b515f7843a
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2023-11-07 20:51:00 -0500

    Add perceptron module

---
+++ src/perceptron.rs
@@ -0,0 +1,37 @@
+mod perceptron {
+
+// shape struct
+struct shape {
+    rows: u32,
+    columns: u32,
+}
+
+// model struct
+struct model {
+    shape: struct shape,
+    weights: Vec<f64>,
+}
+
+// data struct
+struct data {
+    shape: struct shape,
+    elements: Vec<Vec<f64>>,
+    targets: Vec<f64>,
+}
+
+// get shape 
+
+// populate data
+
+// populate model
+
+// fit model 
+
+// sgd
+
+// predict
+
+// evaluate model
+
+
+}
