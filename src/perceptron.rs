c9406528c7a7af1b06c8fc85a2fd460108ba33f1
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2023-11-09 19:14:02 -0500

    Fix struct capitalizations

--- src/perceptron.rs
+++ src/perceptron.rs
@@ -8,13 +8,13 @@ struct Shape {
 
 // model struct
 struct Model {
-    shape: struct shape,
+    shape: Shape,
     weights: Vec<f64>,
 }
 
 // data struct
 struct Data {
-    shape: struct shape,
+    shape: Shape,
     elements: Vec<Vec<f64>>,
     targets: Vec<f64>,
 }
@@ -22,15 +22,15 @@ struct Data {
 // get shape 
 
 // populate data
-impl data {
+impl Data {
     pub fn new(filename: str) -> Self {
 
     }
 }
 
 // populate model
-impl model {
-    pub fn new(data: &data) -> Self {
+impl Model {
+    pub fn new(data: &Data) -> Self {
 
     }
 }
